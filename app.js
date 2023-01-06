84cd80fa01b03cd5de5100d540f6173f8487f86f
Abd El Fattah Amr <amrelfattah.dev@gmail.com>
2023-01-06 04:24:32 +0100

    Bug with API Call

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
+let mainSection = document.querySelector('main'); 
+let title = document.querySelector('.title')
 let countryData = [];
 const getcountry = async () => {
   try {
     const res = await fetch(`https://restcountries.com/v3.1/all`);
     countryData = await res.json();
-  } catch {
+
+    if (!res.ok) {
+      throw new Error("Erreur Serveur");
+    }
+  } catch (e) {
     alert("Problème avec fetch");
+    return;
   }
 };
 
 const DisplayCountries = async () => {
-  await getcountry();
-  console.log(countryData);
+await getcountry() ;
+
+  title.textContent = countryData[40].name.common;
+  mainSection.appendChild(title)
+  
 };
 
 DisplayCountries();
