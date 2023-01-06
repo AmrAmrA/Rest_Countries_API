85e89ca30ab4fd6a24fd34449e1acdd1e8ba055f
Abd El Fattah Amr <amrelfattah.dev@gmail.com>
2023-01-07 00:11:11 +0100

    first display of API Informations

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
-let mainSection = document.querySelector('main'); 
-let title = document.querySelector('.title')
-let countryData = [];
-const getcountry = async () => {
-  try {
-    const res = await fetch(`https://restcountries.com/v3.1/all`);
-    countryData = await res.json();
 
-    if (!res.ok) {
-      throw new Error("Erreur Serveur");
-    }
-  } catch (e) {
-    alert("Problème avec fetch");
-    return;
-  }
-};
+// let countriesSection = document.querySelector(".countries");
+// console.log(countriesSection);
+//  let mainSection = document.querySelector('main');
+//  let title = document.querySelector('.title')
 
-const DisplayCountries = async () => {
-await getcountry() ;
+let mainPart = document.querySelector('main')
+ let countryData = [];
+ const getcountry = async () => {
+   try {
+     const res = await fetch(`https://restcountries.com/v3.1/all`);
+     countryData = await res.json();
 
-  title.textContent = countryData[40].name.common;
-  mainSection.appendChild(title)
-  
-};
+     if (!res.ok) {
+       throw new Error("Erreur Serveur");
+     }
+   } catch (e) {
+     alert("Problème avec fetch");
+     return;
+   }
+ };
+
+ const DisplayCountries = async () => {
+ await getcountry() ;
+console.log(countryData);
+  for (let i = 0; i < countryData.length; i++) {
+      const countriesSection = document.createElement('section'); 
+
+      countriesSection.innerHTML = `
+         <article class="countryCard">
+         <img src="${countryData[i].flags.png}" alt="German flag" class="countryCard__flag">
+         <div class="countryInformations">
+           <p class="countryInformations__name">${countryData[i].name.common}</p>
+           <p class="countryInformations__population"> ${countryData[i].population}</p>
+           <p class="countryInformations__region">${countryData[i].region}</p>
+           <p class="countryInformations__capital">${countryData[i].capital}</p>
+         </div>
+       </article>
+         `;
+
+         mainPart.appendChild(countriesSection)
+     }
+ };
 
 DisplayCountries();
